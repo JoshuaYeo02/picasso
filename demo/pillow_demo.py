042c6dfd07cdca2edb445587c32309bed52220c5
JoshuaYeo02 <joshuayeo99@gmail.com>
2022-06-27 15:02:16 -0700

    Change photo type to P

--- demo/pillow_demo.py
+++ demo/pillow_demo.py
@@ -5,10 +5,9 @@ frames = []
 width, height = 1920, 1080
 baseR, baseG, baseB = 128, 128, 128
 
-im = Image.new('RGB', (width, height), (baseR, baseG, baseB))
+im = Image.new('P', (width, height), (baseR, baseG, baseB))
 draw = ImageDraw.Draw(im)
 frame = im.copy()
-frame = frame.quantize(method=Image.MEDIANCUT)
 frames.append(frame)
 
 for _ in range(50):
@@ -18,7 +17,6 @@ for _ in range(50):
     colR, colG, colB = randint(0, 225), randint(0, 255), randint(0, 255)
     draw.ellipse((ranX - ranR, ranY - ranR, ranX + ranR, ranY + ranR), fill=(colR, colG, colB))
     frame = im.copy()
-    frame = frame.quantize(method=Image.MEDIANCUT)
     frames.append(frame)
 
 frames[5].save('img/pillow_demo.png', quality = 100)
