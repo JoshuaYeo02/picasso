93a7da0574bb94b725a1283d0d665a6937a1da2c
JoshuaYeo02 <joshuayeo99@gmail.com>
2022-07-03 00:30:11 -0700

    Create evolution engine template

---
+++ evolution_engine/evolution.h
@@ -0,0 +1,23 @@
+#ifndef EVOLTUION_H
+#define EVOLTUION_H
+#include "../img_engine/img.h"
+#include <vector>
+
+class Evolution {
+    public:
+        Img target;
+        Img current_frame;
+        vector<vector<int>> pixel_difference;
+        // generates new circle, Return type should not be void
+        void new_frame();
+        // Returns the difference score between current and target img
+        int score_frame();
+        // returns the score between current + a cirlce and target img
+        int score_frame(int x, int y, int radius, int r, int g, int b);
+    public:
+        Evolution(string const path);
+        void natrual_selection(int iterations, string const path);
+
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "evolution_engine/evolution.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+
+}
